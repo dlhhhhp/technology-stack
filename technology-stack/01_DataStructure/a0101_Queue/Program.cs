b16036fbcc4f15d79c82e17c174cae0e5da14e3d
Peng <3946239@qq.com>
2018-07-16 23:55:50 +0800

    添加队列项目

---
+++ technology-stack/01_DataStructure/a0101_Queue/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace a0101_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var systemOut = true;
+            while (systemOut)
+            {
+                Console.WriteLine("请选择队列：1.定长环形数组队列 2.**** 3.***** 0.退出");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        var arrayOne = new QueueFixedLengthArray<string>();
+                        var outOne = true;
+                        var counter = 0;
+                        while (outOne)
+                        {
+                            Console.WriteLine("定长环形数组：" + arrayOne.PrintQueue());
+                            Console.WriteLine("请选择操作：1.入队 2.出队 3.翻倍队列 0.退出");
+                            switch (Console.ReadLine())
+                            {
+                                case "1":
+                                    arrayOne.Enqueue("数据" + (counter++));
+                                    break;
+                                case "2":
+                                    arrayOne.Dequeue();
+                                    break;
+                                case "3":
+                                    arrayOne.DoubleQueueSize();
+                                    break;
+                                case "0":
+                                    outOne = false;
+                                    break;
+                                default: break;
+                            }
+                        }
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "0":
+                        systemOut = false;
+                        break;
+                    default: break;
+                }
+
+            }
+        }
+    }
+}
---
+++ technology-stack/01_DataStructure/a0101_Queue/QueueFixedLengthArray.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace a0101_Queue
+{
+    /// <summary>
+    /// 基于环形数组实现的队列
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class QueueFixedLengthArray<T>
+    {
+        private T[] _array;
+        private int _head;
+        private int _tail;
+        private int _size;
+
+        public QueueFixedLengthArray(int size = 5)
+        {
+            if (size < 2)
+            {
+                Console.WriteLine("队列长度太小，最小要求队列长度为2");
+                return;
+            }
+            _size = size + 1;
+            _array = new T[_size];
+            _head = 1;
+            _tail = 1;
+        }
+
+        public int Count
+        {
+            get { return _size; }
+        }
+
+        public void Enqueue(T item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("队列已满，不能入队");
+                return;
+            }
+            _array[_tail] = item;
+
+            _tail = (_tail + 1) % _size;
+        }
+
+        public void Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("队列已空，不能出队");
+                return;
+            }
+            _array[_head] = default(T);
+
+            _head = (_head + 1) % _size;
+        }
+
+
+        public bool IsFull()
+        {
+            return (_tail + 1) % _size == _head;
+        }
+
+        public bool IsEmpty()
+        {
+            return _head == _tail;
+        }
+
+        public bool DoubleQueueSize()
+        {
+            if (_size < (int)(int.MaxValue / 2))
+            {
+                _size = _size * 2 - 1;
+                T[] temp = _array;
+                _array = new T[_size];
+                for (var i = 0; i < temp.Length; i++)
+                    _array[i] = temp[i];
+                return true;
+            }
+            return false;
+        }
+
+        public string PrintQueue()
+        {
+            StringBuilder build = new StringBuilder("");
+            foreach (var item in _array)
+            {
+                if (item != null)
+                    build.Append(item.ToString() + ",");
+                else
+                    build.Append("null" + ",");
+            }
+            string result = build.ToString();
+            result = result.Remove(result.LastIndexOf(","), ",".Length);
+            return $"队首索引[{_head}]，队尾索引[{_tail}]，队列信息[{result}]";
+        }
+
+    }
+}
